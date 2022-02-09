e408dda6292b18ab76a616ef436c1d961a8bb543
Daniela Brinza <brinza_adriana@yahoo.com>
2022-02-09 19:55:17 +0200

    second commit

--- src/com/itfactory/Repair1.java
+++ src/com/itfactory/Repair1.java
@@ -1,13 +1,12 @@
 package com.itfactory;
 
 public class Repair1 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int x = 3;
         int y = 5;
-        int z = 2;
+        int a = 2;
 
-        double mediaAritmetica = (x+y+z) / 3.0;
+        double mediaAritmetica = (x + y + a) / 3.0;
         System.out.println("Media aritmetica este: " + mediaAritmetica);
 
     }
